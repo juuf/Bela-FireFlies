e2f4453f45925cb990d0aae4b010af4e27ef64b6
juuf <julianpf@uio.no>
2019-05-14 09:09:33 +0200

    adpating render file and pd patch

--- Bela-version (almost working)/default_render.cpp
+++ Bela-version (almost working)/default_render.cpp
@@ -45,13 +45,6 @@ extern "C" {
 #include <string>
 #include <sstream>
 #include <algorithm>
-// my libs
-#include <math.h>
-#include <vector>
-#include <numeric>
-#include <random>
-#include <chrono>
-//#include <iostream>
 
 enum { minFirstDigitalChannel = 10 };
 static unsigned int gAnalogChannelsInUse;
@@ -79,47 +72,9 @@ void Bela_userSettings(BelaInitSettings *settings)
  *  Global variables for tremolo effect applied to libpd output
  */
 
-//float gTremoloRate = 4.0;
-//float gPhase;
-//float gInverseSampleRate;
-
-// function declaration
-double fireflySimulation(int f, double samplerate);
-double median(std::vector<double> medi);
-
-// global intialisation
-//int in = 0;
-//double interactions [1][11]; //interactions[0] = {1,2,3,4,5,6,7,8,9,10,11};
-std::vector<double> E={1};
-std::vector<double> C={1};
-std::vector<double> domega(2,0);
-int fire = 0;
-
-int simlength = 8e4;
-int sr =1e3;
-
-//srand(2345);
-// ((double) rand() / (RAND_MAX))
-
-
-//// initial phase
-//vector<double> temp2={0.191519450378892, 0.622108771039832};
-double pas_phi;// = temp2[0];
-std::vector<double> phi_ext; //= {temp2[1]};
-
-// initial frequency (temp = 2*2.^((rand(2,1)-0.5)*2);)
-//vector<double> temp={1.834587198568937,2.970523429156172};
-double omega;// = temp[0];
-double omega_ext;//= temp[1];
-int f = 0;
-//vector<int> inter;
-int delta_fire=1;
-std::vector<int> df;
-int ii;
-bool count_=true;
-
-#define pi 3.14159265358979323846
-
+float gTremoloRate = 4.0;
+float gPhase;
+float gInverseSampleRate;
 
 /*********/
 
@@ -151,8 +106,8 @@ void dumpMidi()
                gMidiPortNames[n].c_str(),
                midi[n]->isInputEnabled() ? "x" : "_",
                midi[n]->isOutputEnabled() ? "x" : "_",
-               n * 16 + 1,
-               n * 16 + 16
+               n * 16,
+               n * 16 + 15
                );
     }
 }
@@ -330,9 +285,9 @@ void Bela_floatHook(const char *source, float value){
      *  Parse float sent to receiver 'tremoloRate' and assign it to a global variable
      *  N.B. When using libpd receiver names need to be registered (see setup() function below)
      */
-    //    if(strncmp(source, "tremoloRate", 11) == 0){
-    //        gTremoloRate = value;
-    //    }
+    if(strncmp(source, "tremoloRate", 11) == 0){
+        gTremoloRate = value;
+    }
     
     /*********/
     
@@ -385,10 +340,8 @@ static bool pdMultiplexerActive = false;
 
 #ifdef PD_THREADED_IO
 void fdLoop(void* arg){
-    // t_pdinstance* pd_that = (t_pdinstance*)arg;
     while(!gShouldStop){
         sys_doio();
-        //sys_doio(pd_that);
         usleep(3000);
     }
 }
@@ -428,8 +381,8 @@ bool setup(BelaContext *context, void *userData)
      *  Initialise variables for tremolo effect
      */
     
-    //    gInverseSampleRate = 1.0 / context->audioSampleRate;
-    //    gPhase = 0.0;
+    gInverseSampleRate = 1.0 / context->audioSampleRate;
+    gPhase = 0.0;
     
     /*********/
     
@@ -545,56 +498,7 @@ bool setup(BelaContext *context, void *userData)
      *  ------------
      *  Bind an additional receiver for the tremoloRate parameter
      */
-    //    libpd_bind("tremoloRate");
-    
-    
-    // if length of phase > 3333 -> phase(end) = phase(0)
-    
-    //    if (E.size()==0){
-    //    local intialisation
-    //    double omega_ext = temp[1];
-    //    //        int f = 0;
-    //    std::vector<double> omegas;
-    //    std::vector<double> phase;
-    //
-    //    phi_ext.push_back(temp2[1]);
-    //    E.push_back(1);
-    //    C.push_back(1);
-    //
-    //    phase.push_back(pas_phi);
-    //    omegas.push_back(omega);
-    //
-    //    //        count==false;
-    //    //    }
-    //
-    //    //for (int i=2-1; i<simlength; i++) {
-    //    //        ii=i;
-    //    if (phi_ext[phi_ext.size()-1]<1) {
-    //        phi_ext.push_back(phi_ext[phi_ext.size()-1]+omega_ext/sr); //tooth saw -> increase (phase=amplitude)
-    //        if (phi_ext[phi_ext.size()-1]>=1){
-    //            phi_ext[phi_ext.size()-1]=1;
-    //        }
-    //    }
-    //    else{
-    //        f_=(f_+1)%2;
-    //        // std::cout << f_;
-    //        phi_ext.push_back(0);
-    //    }
-    //    phase.push_back(fireflySimulation(f_,sr));
-    //    //    omegas.push_back(omega);
-    //    libpd_start_message(1);
-    //    libpd_add_float(f_);
-    //    libpd_finish_list("firefly");
-    //    //}
-    
-    //    libpd_bang("foo");
-    
-    libpd_start_message(1);
-    libpd_add_float(1);
-    libpd_finish_list("test");
-    
-    
-    
+    libpd_bind("tremoloRate");
     /*********/
     
     // open patch:
@@ -627,147 +531,13 @@ bool setup(BelaContext *context, void *userData)
     Bela_scheduleAuxiliaryTask(fdTask);
 #endif /* PD_THREADED_IO */
     
-    //    dcm.setVerbose(false);
+    dcm.setVerbose(false);
     return true;
 }
-
-
-
-double fireflySimulation(int f, double sr) {
-    // local variable declaration
-    const double Alpha = 0.4;
-    const double Beta = 0.4;
-    const int FilterLength = 8;
-    const double tref = 5;
-    const int DoublingThreshold = 20;
-    double phi;
-    double phiref;
-    double allzeros;
-    std::vector<double> CE;
-    std::vector<double> EE;
-    //    vector<int> res_;
-    
-    delta_fire++;
-    if (pas_phi < 1){
-        phi = pas_phi+omega/sr; //tooth saw -> increase (phase=amplitude)
-        if (phi>=1) {phi=1;}
-    }
-    else{
-        if (fire==1){
-            //            inter.push_back(ii);
-            df.push_back(delta_fire);
-            delta_fire = 0;
-        }
-        fire = (fire+1)%2; //fire only on every other peak
-        phi = 0;
-    }
-    
-    phiref = tref*omega*5e-4;
-    
-    if (phi_ext[phi_ext.size()-1] >= 1){    // if external node is at maximum
-        //        if (f == 0){
-        //        }
-        //        if (f == 1 && fire == 1){ // node j pas_.fires and firing has already occured at this time.
-        //        }
-        
-        if (f == 1 && fire == 0){// if node external node fires and internal firing has not already occured at this time.
-            //            phi_ext[phi_ext.size()-1]  = 1;
-            
-            if (phi > phiref){ // if internal phase is above phiref
-                // PHASE COUPLING FUNCTION
-                phi = phi+Alpha*(-sin(phi*2*pi))*abs(sin(phi*2*pi));
-                if (phi<0){phi=0;} // trim phase to [0 1] range
-                //                else if (phi>=1){phi=1;}
-                if (phi>=1){phi=1;}
-                
-                if (phi < phiref || phi > 1-phiref){
-                    E.push_back(0);
-                }
-                else{
-                    E.push_back((1-cos(2*pi*phi))/2); //else, error is calculated like this
-                }
-                
-                
-                if (E.size() < FilterLength){
-                    C.push_back(median(E));
-                }
-                else{
-                    for(int ii =0;ii<(FilterLength);ii++){
-                        CE.push_back(E[E.size()-FilterLength+ii]);
-                    }
-                    C.push_back(median(CE));
-                }
-                // FREQUENCY COUPLING FUNCTION
-                domega[0] = ((omega * pow(2,Beta*-sin(2*pi*phi)*C[C.size()-1]))+domega[0]*domega[1])/(domega[1]+1);
-                domega[1] = domega[1]+1;
-                if (domega[1] > DoublingThreshold){
-                    omega = 2*omega;
-                    domega = {0,0};
-                }
-            }
-        }
-        
-        if (domega[1] > 0){ // if there has been freq adjustments
-            omega = domega[0];    // Reachback pas_.firefly algorithm
-            domega = {0,0};
-        }
-        
-        
-        if (E.size() < FilterLength){
-            allzeros = std::accumulate(E.begin(), E.end(), 0.0);
-        }
-        else{
-            for(int ii =0;ii<(FilterLength);ii++){
-                EE.push_back(E[E.size()-FilterLength+ii]);
-            }
-            allzeros = std::accumulate(EE.begin(), EE.end(), 0.0);
-        }
-        if (allzeros == 0 && (df.size() > 8)){
-            //            std::sort(inter.begin(), inter.end());
-            //            std::reverse(inter.begin(), inter.end());
-            //            vector<int> res_(inter.size());
-            //            std::adjacent_difference(inter.begin(), inter.end()-(inter.size()-FilterLength), res_.begin()); // difference of vector elements
-            //            double tt = std::accumulate(res_.begin()+1,res_.end(),0); // sum of difference to take the mean afterwards
-            //            omega = -2000*(FilterLength-1)/tt;
-            // better implementation
-            std::vector<int> tmp_;
-            for(int ii=0;ii<(FilterLength-1);ii++){
-                tmp_.push_back(df[df.size()-FilterLength+1+ii]);
-            }
-            double ttt = std::accumulate(tmp_.begin(),tmp_.end(),0);
-            omega = 2000*(FilterLength-1)/ttt;
-        }
-    }
-    if (phi>1){phi=1;}
-    
-    pas_phi = phi;
-    
-    return phi;
-}
-
-
-
-
-double median(std::vector<double> medi)
-{
-    std::sort(medi.begin(), medi.end());     // sort values
-    
-    double tmedian;
-    if (medi.size() % 2 == 0)           // even
-        tmedian = (medi[medi.size() / 2 - 1] + medi[medi.size() / 2]) / 2;
-    else                                // odd
-        tmedian = medi[medi.size() / 2];
-    
-    return tmedian;
-}
-
-
-
 
 void render(BelaContext *context, void *userData)
 {
     int num;
-    
 #ifdef PARSE_MIDI
     for(unsigned int port = 0; port < midi.size(); ++port){
         while((num = midi[port]->getParser()->numAvailableMessages()) > 0){
@@ -952,85 +722,23 @@ void render(BelaContext *context, void *userData)
          *  output to Bela output buffer
          */
         
-        //for (j = 0, p0 = gOutBuf; j < gLibpdBlockSize; j++, p0++) {
-        //    // Generate a sinewave with frequency set by gTremoloRate
-        //    // and amplitude from -0.5 to 0.5
-        //    float lfo = sinf(gPhase) * 0.5;
-        //    // Keep track and wrap the phase of the sinewave
-        //    gPhase += 2.0 * M_PI * gTremoloRate * gInverseSampleRate;
-        //    if(gPhase > 2.0 * M_PI)
-        //        gPhase -= 2.0 * M_PI;
-        //    for (k = 0, p1 = p0; k < context->audioOutChannels; k++, p1 += gLibpdBlockSize) {
-        //        // *p1 here is the sample in Pd's buffer which
-        //        // corresponds to the jth frame of the kth channel
-        //        // we edit its value in place
-        //        *p1 = *p1 * lfo;
-        //    }
-        //}
-        
-        /*********/
-        
-        
-        //std::vector<double> omegas;
-        //std::vector<double> phase;
-        
-        //    std::vector<double> temp2;
-        ////    for (int k_=0; k_ <= 1; k_++){
-        ////        double b =((double) rand()/(RAND_MAX));
-        ////        temp2.push_back(b);
-        ////        //    cout << myVector[b] << endl;
-        ////    }
-        //
-        if (count_==true){
-            // random phase and frequency intialisation
-            std::mt19937_64 rng;
-            // initialize the random number generator with time-dependent seed
-            uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-            std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
-            rng.seed(ss);
-            // initialise a uniform distribution between 0 and 1
-            std::uniform_real_distribution<double> unif(0, 1);
-            // phase intialisation
-            pas_phi = unif(rng);
-            phi_ext.push_back(unif(rng));
-            // frequency intialisation
-            std::uniform_real_distribution<double> unif_2(0,1);
-            omega = 2*pow(2,(unif_2(rng)-0.5)*2);
-            omega_ext= 2*pow(2,(unif_2(rng)-0.5)*2);
-            count_=false;  // only once
-        }
-        
-        // phase.push_back(pas_phi);
-        //omegas.push_back(omega);
-        
-        
-        //  for (int i=2-1; i<simlength; i++) {
-        //  ii=i;
-        if (phi_ext[phi_ext.size()-1]<1) {
-            phi_ext.push_back(phi_ext[phi_ext.size()-1]+omega_ext/sr); //tooth saw -> increase (phase=amplitude)
-            if (phi_ext[phi_ext.size()-1]>=1){
-                phi_ext[phi_ext.size()-1]=1;
+        for (j = 0, p0 = gOutBuf; j < gLibpdBlockSize; j++, p0++) {
+            // Generate a sinewave with frequency set by gTremoloRate
+            // and amplitude from -0.5 to 0.5
+            float lfo = sinf(gPhase) * 0.5;
+            // Keep track and wrap the phase of the sinewave
+            gPhase += 2.0 * M_PI * gTremoloRate * gInverseSampleRate;
+            if(gPhase > 2.0 * M_PI)
+                gPhase -= 2.0 * M_PI;
+            for (k = 0, p1 = p0; k < context->audioOutChannels; k++, p1 += gLibpdBlockSize) {
+                // *p1 here is the sample in Pd's buffer which
+                // corresponds to the jth frame of the kth channel
+                // we edit its value in place
+                *p1 = *p1 * lfo;
             }
         }
-        else{
-            f=(f+1)%2;
-            //              std::cout << f;
-            phi_ext.push_back(0);
-        }
-        // phase.push_back(fireflySimulation(f,sr));
-        //   omegas.push_back(omega);
-        //}
-        //        //libpd_bang("foo");
-        //
-        //        //libpd_start_message(1);
-        //        //libpd_add_float(1);
-        //        //libpd_finish_list("test");
         
-        libpd_start_message(1);
-        libpd_add_float(phi_ext[phi_ext.size()-1]);
-        //libpd_add_float(0);
-        libpd_finish_list("test");
-        
+        /*********/
         
         // audio output
         for(int n = 0; n < context->audioOutChannels; ++n)
